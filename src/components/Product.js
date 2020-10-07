420b4d9a004415e14147a4a84f319be8808b2094
Gabriel Milhomem <gabriell.mil@gmail.com>
2020-10-07 19:35:32 -0300

    Logica Contador

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,11 +3,25 @@ import React from "react";
 export default function Product(props) {
     var {type, image, name, price, text} = props;
     var [selected, setSelected] = React.useState(false);
+    var [numItem, setNumItem] = React.useState(1);
     var articleClass = selected ? `${type} green-border`: type;
     var counterClass = selected ? "counter show": "counter";
 
+    function decrement(event) {
+        var num = numItem - 1;
+
+        if (num === 0) {
+            event.stopPropagation();
+            setSelected(false);
+            setNumItem(1);
+            return ;
+        }
+
+        setNumItem(num);
+    }
+
     return (
-        <article className= {articleClass} onClick= {() => setSelected(true)}> 
+        <article className= {articleClass} onClick= {(event) => setSelected(true)}> 
             <img src= {`./images/${type}/${image}`} alt= {name} />
             <h2> {name} </h2>
             <p> {text} </p>
@@ -15,14 +29,11 @@ export default function Product(props) {
                 <span> {`R$ ${price}`} </span>
 
                 <div className= {counterClass}>
-                    <span className= "minus"> - </span>
-                    <strong> 0 </strong>
-                    <span className= "add"> + </span>
+                    <span className= "minus" onClick= {(event) => decrement(event)}> - </span>
+                    <strong> {numItem} </strong>
+                    <span className= "add" onClick= {() => setNumItem(numItem + 1)} > + </span>
                 </div>
-
-
             </div>
-            
         </article>
     );
 }
